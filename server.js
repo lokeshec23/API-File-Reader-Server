90b80a33229774b55bede2120743ac81b6c33db2
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-09-15 12:23:06 +0530

    new changes

--- server.js
+++ server.js
@@ -30,20 +30,29 @@
 //         console.log(`server running on ${port}`);
 //     }
 // })
+
 const fs = require('fs');
 const http = require('http');
 const port = 2001;
+const express = require('express');
 
- 
+const app = express();
 
-const server = http.createServer((req, res) => {
+app.listen(port, (err) => {
+    if (err) {
+        console.log('Something went wrong:', err);
+    } else {
+        console.log(`Server is running on port ${port}`);
+    }
+});
+
+app.get('/createFile', (req, res) => {
     try {
+
         // To create files inside API-FILE
         for (let i = 0; i < 5; i++) {
             fs.writeFileSync(`./API-FILE/file-${i + 1}.txt`, 'Hello World');
         }
-
- 
 
         // To read file names
         fs.readdir(`./API-FILE`, (err, files) => {
@@ -63,14 +72,41 @@ const server = http.createServer((req, res) => {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
     }
-});
+})
 
- 
+// const server = http.createServer((req, res) => {
+//     try {
 
-server.listen(port, (err) => {
-    if (err) {
-        console.log('Something went wrong:', err);
-    } else {
-        console.log(`Server is running on port ${port}`);
-    }
-});
+//         // To create files inside API-FILE
+//         for (let i = 0; i < 5; i++) {
+//             fs.writeFileSync(`./API-FILE/file-${i + 1}.txt`, 'Hello World');
+//         }
+
+//         // To read file names
+//         fs.readdir(`./API-FILE`, (err, files) => {
+//             if (err) {
+//                 console.error('Error reading directory:', err);
+//                 res.writeHead(500, { 'Content-Type': 'text/plain' });
+//                 res.end('Internal Server Error');
+//             } else {
+//                 files.forEach((file) => {
+//                     res.write(`${file}\n`);
+//                 });
+//                 res.end();
+//             }
+//         });
+//     } catch (error) {
+//         console.error('Error:', error);
+//         res.writeHead(500, { 'Content-Type': 'text/plain' });
+//         res.end('Internal Server Error');
+//     }
+// });
+
+
+// server.listen(port, (err) => {
+//     if (err) {
+//         console.log('Something went wrong:', err);
+//     } else {
+//         console.log(`Server is running on port ${port}`);
+//     }
+// });
