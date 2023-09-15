0f0f524abe4f6243253dbb596708b6839a38dfa6
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-09-15 11:56:50 +0530

    a

--- main.js
+++
@@ -1,6 +0,0 @@
-// const fs = require('fs')
-
-// // To create files inside API_File
-// for(let i=0; i<5; i++){
-//     fs.writeFileSync(`./API-FILE/file-${i+1}.txt`,'Hello World');
-// }
--- server.js
+++ server.js
@@ -1,31 +1,76 @@
 
-const fs = require('fs')
+// const fs = require('fs')
+// const http = require('http');
+// const port = 2001;
+
+// // const path = require('path');
+// // const dirPath = path.join(__dirname,'API-FILE');
+
+
+// const server = http.createServer((req,res)=>{
+    
+//     // To create files inside API_File
+//     for(let i=0; i<5; i++){
+//         fs.writeFileSync(`./API-FILE/file-${i+1}.txt`,'Hello World');
+//     }
+
+//     // To read file names
+//     fs.readdir(`./API-FILE`,(err,files)=>{
+//         files.map((file)=>{
+//             res.write(`${file}\n`);
+//         })
+//         res.end()    
+//     })
+// })
+
+// server.listen(port,(err)=>{
+//     if(err){
+//         console.log('something went bad');
+//     }else{
+//         console.log(`server running on ${port}`);
+//     }
+// })
+const fs = require('fs');
 const http = require('http');
 const port = 2001;
 
-// const path = require('path');
-// const dirPath = path.join(__dirname,'API-FILE');
+ 
 
+const server = http.createServer((req, res) => {
+    try {
+        // To create files inside API-FILE
+        for (let i = 0; i < 5; i++) {
+            fs.writeFileSync(`./API-FILE/file-${i + 1}.txt`, 'Hello World');
+        }
 
-const server = http.createServer((req,res)=>{
-    
-    // To create files inside API_File
-    for(let i=0; i<5; i++){
-        fs.writeFileSync(`./API-FILE/file-${i+1}.txt`,'Hello World');
+ 
+
+        // To read file names
+        fs.readdir(`./API-FILE`, (err, files) => {
+            if (err) {
+                console.error('Error reading directory:', err);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                files.forEach((file) => {
+                    res.write(`${file}\n`);
+                });
+                res.end();
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
     }
-    // To read file names
-    fs.readdir(`./API-FILE`,(err,files)=>{
-        files.map((file)=>{
-            res.write(`${file}\n`);
-        })
-        res.end()    
-    })
-})
+});
 
-server.listen(port,(err)=>{
-    if(err){
-        console.log('something went bad');
-    }else{
-        console.log(`server running on ${port}`);
+ 
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log('Something went wrong:', err);
+    } else {
+        console.log(`Server is running on port ${port}`);
     }
-})
+});
